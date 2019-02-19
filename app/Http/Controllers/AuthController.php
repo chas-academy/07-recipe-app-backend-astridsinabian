10ed283cb2588f9c6dbca386adcc784689dcfada
Astrid <astrid.sinabian@chasacademy.se>
2019-02-19 14:52:24 +0100

    Add login and register functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\User;
 
 class AuthController extends Controller
 {
@@ -14,7 +16,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
     }
 
     /**
@@ -27,10 +29,16 @@ class AuthController extends Controller
         $credentials = request(['email', 'password']);
 
         if (! $token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['error' => 'Invalid email or password'], 401);
         }
 
         return $this->respondWithToken($token);
+    }
+
+    public function register(RegisterRequest $request) 
+    {
+        $user = User::create($request->all());
+        return $this->login($request);
     }
 
     /**
